b8b7da1f466f23f94dcb0ae242480dbac4d25e7c
kenttolzmann <tolzmank@oregonstate.edu>
2024-11-18 14:42:52 -1000

    microservice A and test program complete

--- anshu_microservice_a/anshu_micro_listener.py
+++ anshu_microservice_a/anshu_micro_listener.py
@@ -1,61 +1,123 @@
 # Kent Tolzmann
-# CS361 - Assignment 4 - ZeroMQ Spike - Listener
+# CS361 - Microservice A
 # 10/28/24
 
-# lms_microservice_a():
-# authentic_user():
-#
 
-def message_logger():
-
-    print('message_logger MICROSERVICE is running....')
+def lms_microservice():
+    print('LMS MICROSERVICE is running....')
     import zmq
     import json
 
     context = zmq.Context()
     socket = context.socket(zmq.REP)
-    socket.bind("tcp://*:5555")
+    socket.bind("tcp://*:5554")
 
+    users = []
+    books = []
+    messages = []
 
-    message_log = []    # [  [ {'segments': [{},{},{}], 'id': 0} ], [ {'segments': [{},{},{}] , 'id': 1} ]  ]
     while True:
-        message = socket.recv()    # [ {'segments': [{},{},{}], 'id': 0} ]
+        message = socket.recv()     # [{'sign_up': True}, {'username': 'John_Doe','password': 'password123','email': 'user@example.com', 'id': 3}]
         data = json.loads(message.decode())
         print(f"Received: {message}")
+        operation = data[0]
+        user_operations = [{'sign_up': True}, {'sign_in': True}, {'delete_user_id': True}]
+        book_operations = [{'store_book': True}, {'borrow_book': True}, {'delete_book_id': True}]
 
-        # delete all messages
-        if data[0] == {'clear_message_log': True}:
-            print('CLEAR message log')
-            message_log = []
+        # User Authentication
+        if operation in user_operations:
+            user = data[1]
+            reply = user_authentication(operation, user, users)
 
-        # delete target id message
-        elif data[0] == {'delete_message_id': True}:
-            print('DELETE TARGET message')
-            message_id = data[1]
-            for message in message_log:
-                if message[0]['id'] == message_id:
-                    message_log.remove(message)
-                    break
+        # Books CRUD Operations
+        elif operation in book_operations:
+            book = data[1]
+            reply = book_ops(operation, book, books)
 
-        # edit target id message
-        elif data[0] == {'edit_message_id': True}:
-            print('EDIT message')
-            message_id = data[1]
-            updated_message = data[2]
-            for i in range(len(message_log)):
-                if message_log[i][0]['id'] == message_id:
-                    message_log[i] = updated_message
-                    break
-
-        # create new message
+        # Message storing
         else:
-            print('CREATE message')
-            message_log.append(data)
+            message = data[0]
+            messages.append(message)
+            reply = messages
 
-        response = json.dumps(message_log)
+        response = json.dumps(reply)
+        print(f'Response: {response}')
         socket.send(response.encode())
-        print(f"updated message log:\n {message_log}")
+        print(f"users:\n {users}")
+        print(f"books:\n {books}")
 
 
-message_logger()
+def user_authentication(operation, user, users):
+    # Add user
+    if operation == {'sign_up': True}:
+        print('USER SIGN UP')
+        new_user = user
+        for user in users:
+            if new_user['username'] == user['username']:
+                return [{'sign_up': 'username already exists'}]
+        else:
+            users.append(new_user)
+            print(f'user login successful: {new_user['username']}')
+            return users
 
+    # Authenticate user
+    elif operation == {'sign_in': True}:
+        print('USER SIGN IN')
+        auth_user = user
+        for user in users:
+            if auth_user['username'] == user['username'] and auth_user['password'] == user['password']:
+                return [{'sign_in': True}]
+        else:
+            return [{'sign_in': False}]
+
+    # Delete user
+    elif operation == {'delete_user_id': True}:
+        print('DELETE USER')
+        user_id = user
+        for user in users:
+            if user['id'] == user_id:
+                users.remove(user)
+                return users
+        return [{'delete_user_id': 'user id not found'}]
+
+
+def book_ops(operation, book, books):
+    # Add book
+    if operation == {'store_book': True}:
+        print('STORE BOOK')
+        new_book = book
+        for book in books:
+            if new_book['title'] == book['title']:
+                return [{'store_book': 'book name already exists'}]
+        else:
+            books.append(new_book)
+            return books
+
+    # Borrow book
+    elif operation == {'borrow_book': True}:
+        print('BORROW BOOK')
+        book_id = book
+        for book in books:
+            if book['id'] == book_id:
+                book['available'] = False
+                return books
+        return [{'borrow_book': 'book id not found'}]
+
+    # Delete book
+    elif operation == {'delete_book_id': True}:
+        print('DELETE BOOK ID')
+        book_id = book
+        for book in books:
+            if book['id'] == book_id:
+                books.remove(book)
+                return books
+        return [{'delete_book_id': 'book id not found'}]
+
+    elif operation == {'delete_all_books': True}:
+        print('DELETE ALL BOOKS')
+        books = []
+        return books
+
+
+
+lms_microservice()
--- anshu_microservice_a/anshu_test_request_data.py
+++ anshu_microservice_a/anshu_test_request_data.py
@@ -1,72 +1,6 @@
 # Author: Kent Tolzmann
-# CS361 Course Project - Emphasis Selector App
-# 11/4/2024
-import json
-
-from flask import Flask, render_template, redirect, url_for, request
-app = Flask(__name__)
-
-messages = []   # [ [{},{},{}], [{}] ]
-id_counter = 0
-
-
-@app.route('/')
-def index():
-    return render_template('index.html', messages=messages)
-
-
-@app.route('/text_entry', methods=['POST'])
-def text_entry():
-    print('TEXT ENTRY action triggered')
-    global id_counter
-    segments = json.loads(request.form['all_text_segments'])  # [{},{},{}]
-    export_message = {
-        'segments': segments,
-        'id': id_counter
-    }
-    print('message exported: ', [export_message])
-
-    global messages
-    messages = get_micro_response([export_message])
-    id_counter += 1
-    return redirect(url_for('index'))
-
-
-@app.route('/edit_message/<int:message_id>', methods=['POST'])
-def edit_message(message_id):
-    print('EDIT Message triggered')
-    updated_message = {
-        'text': request.form['user_input'],
-        'font_size': request.form['font_size'],
-        'font_weight': request.form['font_weight'],
-        'font_style': request.form['font_style'],
-        'text_decoration': request.form['text_decoration'],
-        'id': message_id
-    }
-    edit_request = [{'edit_message_id': True}, message_id, updated_message]
-    global messages
-    messages = get_micro_response(edit_request)
-    return redirect(url_for('index'))
-
-
-@app.route('/delete_message/<int:message_id>', methods=['POST'])
-def delete_message(message_id):
-    print(f'DELETE message action triggered: {message_id}')
-    delete_request = [{'delete_message_id': True}, message_id]
-    global messages
-    messages = get_micro_response(delete_request)
-    global id_counter
-    return redirect(url_for('index'))
-
-
-@app.route('/clear_messages')
-def clear_messages():
-    print('CLEAR action triggered')
-    global messages
-    global id_counter
-    messages = get_micro_response([{'clear_message_log': True}])
-    id_counter = 0
-    return redirect(url_for("index"))
+# CS361 Microservice A
+# 11/18/2024
 
 
 def get_micro_response(json_obj):
@@ -74,11 +8,13 @@ def get_micro_response(json_obj):
     import json
     context = zmq.Context()
 
-    print("Connecting to Emphasis Selector microservice server...")
+    print("Connecting to Microservice A server...")
     socket = context.socket(zmq.REQ)
-    socket.connect("tcp://localhost:5555")
+    socket.connect("tcp://localhost:5554")
+
     response = json.dumps(json_obj)
     socket.send(response.encode())
+    print(f'Sent Request:\n {response}')
 
     message = socket.recv()
     print(f"Received Reply:\n {message.decode()}")
@@ -86,9 +22,71 @@ def get_micro_response(json_obj):
     return json_obj_message
 
 
-def live_text_stage():
-    return
-
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # example requests
+    reply = []
+    while True:
+        print()
+        item_type = input('user, book, or message? ')
+
+        if item_type == 'user':
+            operation = input('Enter operation request: ')
+            if operation != 'delete_user_id':
+                username = input('Username: ')
+                password = input('Password: ')
+                email = input('Email: ')
+                user_id = int(input('ID: '))
+                item = {
+                    'username': username,
+                    'password': password,
+                    'email': email,
+                    'id': user_id
+                }
+                reply = [{operation: True}, item]
+            else:
+                user_id = int(input('user ID: '))
+                reply = [{operation: True}, user_id]
+
+        elif item_type == 'book':
+            operation = input('Enter operation request: ')
+            if operation == 'store_book':
+                title = input('Title: ')
+                author = input('Author: ')
+                book_id = int(input('ID: '))
+                isbn = input('ISBN: ')
+                available = True
+                item = {
+                    'title': title,
+                    'author': author,
+                    'id': book_id,
+                    'isbn': isbn,
+                    'available': available
+                }
+                reply = [{operation: True}, item]
+
+            else:
+                book_id = int(input('Book ID: '))
+                reply = [{operation: True}, book_id]
+
+
+        else:
+            print('New message input: \n')
+            key1 = input('Key 1: ')
+            valueA = input('Value A: ')
+            key2 = input('Key 2: ')
+            valueB = input('Value B: ')
+            message_id = int(input('ID: '))
+            item = {
+                key1: valueA,
+                key2: valueB,
+                'id': message_id
+            }
+            reply = [item]
+
+
+        get_micro_response(reply)
+
+
+
+
